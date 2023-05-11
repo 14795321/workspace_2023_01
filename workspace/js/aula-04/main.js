a177ef121aadb998bf6b0074730b327645342e6f
PedroP21 <pedro.patricio.rds21@gmail.com>
2023-05-10 21:49:08 -0300

    aula 4 js

---
+++ workspace/js/aula-04/main.js
@@ -0,0 +1,9 @@
+var notaMarcos = 2
+var notaMinima = 7 
+
+if (notaMarcos >= notaMinima) { //false
+    console.log("você está aprovado")}
+    else{
+        console.log("você está reprovado")
+    }
+    
