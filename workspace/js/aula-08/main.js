d24b40319d01ffb8f859abb4cbd738929ab93087
PedroP21 <pedro.patricio.rds21@gmail.com>
2023-05-18 20:31:40 -0300

    aula 08

--- workspace/js/aula-08/main.js
+++ workspace/js/aula-08/main.js
@@ -9,3 +9,18 @@ console.log(z);
 function myFunc(num1, num2) {
     return num1 * num2;
 }
+
+//Arrow functions
+console.clear();
+const hello = () => {
+    return 'Olá Arrow Function!';
+};
+console.log(hello); // Retorna o objeto função
+console.log(hello()); // Executa a função e imprime a string no return
+
+console.clear();
+const addNums2 = (num1 = 1, num2 = 1) => {
+return num1 + num2;
+};
+let soma = addNums2(5, 10);
+console.log(soma);
