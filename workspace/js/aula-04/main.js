c1b3cbd7a350d3ab4f285b40599e21fa21dc950c
PedroP21 <pedro.patricio.rds21@gmail.com>
2023-05-18 19:07:46 -0300

    subindo

--- workspace/js/aula-04/main.js
+++ workspace/js/aula-04/main.js
@@ -1,9 +1,12 @@
-var notaMarcos = 2
-var notaMinima = 7 
-
-if (notaMarcos >= notaMinima) { //false
-    console.log("você está aprovado")}
-    else{
-        console.log("você está reprovado")
-    }
+// Funções
+function addNums(num1 = 1, num2 = 1) {
+    return num1 + num2;
+   }
+   let x = addNums(4, 5); // Chamada da função addNums
+   console.log(x);
+   let z = myFunc(4, 5); // Chamada da função myFunc
+   console.log(z);
+   function myFunc(num1, num2) {
+    return num1 * num2;
+   }
     
